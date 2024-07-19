52e8a6d9a053d789e5abdeb20797ae0a5c7d9870
SiquaraDev <matheus.siquara99@gmail.com>
2024-07-19 12:43:51 -0300

    dynamic cart number

--- src/components/template/Cart.tsx
+++ src/components/template/Cart.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
-import Logo from "./Logo";
 import { IconShoppingCart } from "@tabler/icons-react";
+import useCart from "@/data/hooks/useCart";
 
 export default function Cart() {
+    const { itemsQuantity } = useCart();
+
     return (
         <Link href="/cart">
             <div className="flex relative">
                 <IconShoppingCart size={32} stroke={1} />
-                <div className="absolute w-6 h-6 bg-red-500 rounded-full -top-2.5 -right-2.5 flex justify-center items-center text-xs">
-                    99
-                </div>
+                {itemsQuantity ? (
+                    <div className="absolute w-6 h-6 bg-red-500 rounded-full -top-2.5 -right-2.5 flex justify-center items-center text-xs">
+                        {itemsQuantity}
+                    </div>
+                ) : null}
             </div>
         </Link>
     );
